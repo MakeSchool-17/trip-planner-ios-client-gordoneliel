1d8512ac3a1b0752ca752c03a0e0fe43f41b7539
gordoneliel <gordoneliel@gmail.com>
2015-10-15 16:05:27 -0700

    Experimenting with guard, defer

--- TripPlanner/TripPlanner/ViewControllers/AddTripViewController.swift
+++ TripPlanner/TripPlanner/ViewControllers/AddTripViewController.swift
@@ -18,10 +18,27 @@ class AddTripViewController: UIViewController {
 
     weak var tripAddDelegate: AddTripDelegate?
     
+    @IBOutlet weak var tripNameField: UITextField!
+    
+    enum DataError : ErrorType {
+        case EmptyText
+        // add more later
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func addTrip() throws {
+        
+        defer {tripAddDelegate?.tripAddFinished(self, trip: "")}
+        
+        guard let tripName = tripNameField.text else{
+            throw DataError.EmptyText
+        }
+        
+        tripAddDelegate?.tripAddFinished(self, trip: tripName)
+    }
 }
 
 
